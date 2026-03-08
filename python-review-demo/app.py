eca0b7c2970ab3dc667c052ea62f266f38d2d95f
vsengar-79 <146079793+vsengar-79@users.noreply.github.com>
2026-03-08 18:13:48 +0530

    testing
    Fix typo in import statement and comment out user service imports

--- python-review-demo/app.py
+++ python-review-demo/app.py
@@ -1,5 +1,5 @@
-from flask import Flask, request, jsonify
-from services.user_service import get_user, create_user
+frm flask import Flask, request, jsonify
+# from services.user_service import get_user, create_user
 
 app = Flask(__name__)
 
