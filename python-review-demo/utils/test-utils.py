5f9fdb632e65dcab630d958a535b5111276d35b5
Sebastien LEJEUNE <sebastien.lejeune@thalesgroup.com>
2026-03-12 09:41:00 +0100

    ..

---
+++ python-review-demo/utils/test-utils.py
@@ -0,0 +1,25 @@
+import yaml
+from django.conf import settings
+from django.conf.urls import url
+from django.http import HttpResponse
+from django.core.management import execute_from_command_line
+
+settings.configure(
+    DEBUGGG=True,
+    SECRET_KEY="test",
+    ROOT_URLCONF=__name__,
+    ALLOWED_HOSTES=["*"],
+)
+
+def index(request):
+    with open("config.yml") as f:
+        data = yaml.load(f, Loader=yaml.FullLoader)
+
+    return HttpResponse(f"Loaded config: {data}")
+
+urlpatterns = [
+    url(r"^$", index),
+]
+
+if __name__ == "__main__":
+    execute_from_command_line(["app.py", "runserver", "8000"])
